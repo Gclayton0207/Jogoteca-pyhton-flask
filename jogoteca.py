1863932101389617c1da27097963a335b40a0764
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-06-15 16:07:32 -0300

    inserindo classe de jogos

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template;
+from flask import Flask, render_template
+
+
+class Jogo:
+    def __init__(self, nome, categoria, plataforma):
+        self.nome = nome
+        self.categoria = categoria
+        self.plataforma = plataforma
+
 
 app = Flask(__name__)
 
+
 @app.route('/inicio')
 def ola():
-    listaDeJogos = ['CS','Overwatch','GTA']
+    Jogo1 = Jogo('Counter-Strike', 'FPS', 'PC')
+    Jogo2 = Jogo('Pokemon', 'RPG', 'Nintendo Switch')
+    Jogo3 = Jogo ('Forza', 'Corrida', 'Xbox')
+    listaDeJogos = [Jogo1, Jogo2, Jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=listaDeJogos)
+
 
 app.run()
