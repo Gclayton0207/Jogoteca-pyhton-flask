e6778981f4265a8101078a7b49a07e1d29fb0322
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-06-15 16:19:28 -0300

    inserindo nova rota

--- jogoteca.py
+++ jogoteca.py
@@ -15,9 +15,12 @@ app = Flask(__name__)
 def ola():
     Jogo1 = Jogo('Counter-Strike', 'FPS', 'PC')
     Jogo2 = Jogo('Pokemon', 'RPG', 'Nintendo Switch')
-    Jogo3 = Jogo ('Forza', 'Corrida', 'Xbox')
+    Jogo3 = Jogo('Forza', 'Corrida', 'Xbox')
     listaDeJogos = [Jogo1, Jogo2, Jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=listaDeJogos)
 
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo jogo')
 
 app.run()
