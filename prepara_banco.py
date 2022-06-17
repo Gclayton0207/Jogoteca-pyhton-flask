982de8b20827d9025506fee8c06c39825da26fc9
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-06-17 10:16:48 -0300

    feat: criando banco de dados

---
+++ prepara_banco.py
@@ -0,0 +1,93 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+print("Conectando...")
+try:
+      conn = mysql.connector.connect(
+            host='127.0.0.1',
+            user='root',
+            password='number1245'
+      )
+except mysql.connector.Error as err:
+      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('Existe algo errado no nome de usuário ou senha')
+      else:
+            print(err)
+
+cursor = conn.cursor()
+
+cursor.execute("DROP DATABASE IF EXISTS `jogoteca`;")
+
+cursor.execute("CREATE DATABASE `jogoteca`;")
+
+cursor.execute("USE `jogoteca`;")
+
+# criando tabelas
+TABLES = {}
+TABLES['Jogos'] = ('''
+      CREATE TABLE `jogos` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `nome` varchar(50) NOT NULL,
+      `categoria` varchar(40) NOT NULL,
+      `plataforma` varchar(20) NOT NULL,
+      PRIMARY KEY (`id`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
+
+TABLES['Usuarios'] = ('''
+      CREATE TABLE `usuarios` (
+      `nome` varchar(20) NOT NULL,
+      `nickname` varchar(8) NOT NULL,
+      `senha` varchar(100) NOT NULL,
+      PRIMARY KEY (`nickname`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
+
+for tabela_nome in TABLES:
+      tabela_sql = TABLES[tabela_nome]
+      try:
+            print('Criando tabela {}:'.format(tabela_nome), end=' ')
+            cursor.execute(tabela_sql)
+      except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                  print('Já existe')
+            else:
+                  print(err.msg)
+      else:
+            print('OK')
+
+
+# inserindo usuarios
+usuario_sql = 'INSERT INTO usuarios (nome, nickname, senha) VALUES (%s, %s, %s)'
+usuarios = [
+      ("Giovanni Clayton", "Gclayton", "alohomora"),
+      ("Juliana Melo", "Juuh", "alohomora"),
+      ("Bartolomeu", "Barto", "jade")
+]
+cursor.executemany(usuario_sql, usuarios)
+
+cursor.execute('select * from jogoteca.usuarios')
+print(' -------------  Usuários:  -------------')
+for user in cursor.fetchall():
+    print(user[1])
+
+# inserindo jogos
+jogos_sql = 'INSERT INTO jogos (nome, categoria, plataforma) VALUES (%s, %s, %s)'
+jogos = [
+      ('Counter-Strike', 'FPS', 'PC'),
+      ('God of War', 'Hack n Slash', 'PS2'),
+      ('Mortal Kombat', 'Luta', 'PS2'),
+      ('Valorant', 'FPS', 'PC'),
+      ('Pokemon', 'RPG', 'Nintendo Switch'),
+      ('Need for Speed', 'Corrida', 'PS2'),
+]
+cursor.executemany(jogos_sql, jogos)
+
+cursor.execute('select * from jogoteca.jogos')
+print(' -------------  Jogos:  -------------')
+for jogo in cursor.fetchall():
+    print(jogo[1])
+
+# commitando se não nada tem efeito
+conn.commit()
+
+cursor.close()
+conn.close()
