84b3af888657c36672dfe480d7a2be034290a454
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-06-17 10:37:32 -0300

    feat: criando classes das tabelas

--- jogoteca.py
+++ jogoteca.py
@@ -1,35 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-
-class Jogo:
-    def __init__(self, nome, categoria, plataforma):
-        self.nome = nome
-        self.categoria = categoria
-        self.plataforma = plataforma
-
-
-Jogo1 = Jogo('Counter-Strike', 'FPS', 'PC')
-Jogo2 = Jogo('Pokemon', 'RPG', 'Nintendo Switch')
-Jogo3 = Jogo('Forza', 'Corrida', 'Xbox')
-listaDeJogos = [Jogo1, Jogo2, Jogo3]
-
-
-class Usuario:
-    def __init__(self, nome, nickname, senha):
-        self.nome = nome
-        self.nickname = nickname
-        self.senha = senha
-
-
-usuario1 = Usuario("Giovanni Clayton", "Gclayton", "alohomora")
-usuario2 = Usuario("Juliana Melo", "Juuh", "alohomora")
-usuario3 = Usuario("Batolomeu", "Barto", "jade")
-
-usuarios = {usuario1.nickname: usuario1,
-            usuario2.nickname: usuario2,
-            usuario3.nickname: usuario3}
-
 app = Flask(__name__)
 app.secret_key = 'claytinho'
 
@@ -44,6 +15,22 @@ app.config['SQLAlCHEMY_DATABASE_URI'] = \
 
 db = SQLAlchemy(app)
 
+class Jogos(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoIncrement=True)
+    nome = db.Column(db.string(50), nullable=False)
+    categoria = db.Column(db.string(40), nullable=False)
+    plataforma = db.Column(db.string(20), nullable=False)
+    
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+class Usuarios(db.Model):
+    nickname = db.Column(db.string(8), primary_key=True)
+    nome = db.Column(db.string(20), nullable=False)
+    senha = db.Column(db.string(100), nullable=False)
+    
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 @app.route('/')
 def index():
